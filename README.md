726a372b01c6e2459815d9f4b4f067248a9b7082
Dimarr <34514263+Dimarr@users.noreply.github.com>
2023-03-05 19:04:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-"# TestParsing" 
+# TestParsing
+
+## Supported Operating Systems
+- **Linux (Bash):**	CentOS, Debian, Ubuntu
+- **Windows (PowerShell):**	10, 2012, 2016
+
+## Prerequisites
+- Python 3.9+
+```shell
+pip install -r requirements.txt
+```
+## Installation
+1. Download **testparsing.zip** from GitHub
+2. Unzip 
+
+## Usage
+**Using command-line arguments only:**
+```shell
+parser_excel.py --input FileName --output OutputForTotal --mode APPEND/OVERWRITE --delcompany CompanyName
+```
+**Note:**
+1. FileName is name of Excel file for parsing.
+2. OutputForTotal is name of output Excel File with total calculation
+3. APPEND/OVERWRITE :
+    1. In case **APPEND** data will be added into database 
+    2. In case **OVERWRITE** data will be replaced in database
+4. CompanyName is name of company which should be deleted 
--- parser_excel.py
+++ parser_excel.py
@@ -7,7 +7,7 @@ import pandas
 db_name = "result_db.sqlite3"
 mnth = "02"
 year = "2023"
-types = ["fact", "forecast"]
+types = ["fact", "forecast"]  # The type of possible values
 
 global Db
 Db = Database(db_name)
@@ -15,14 +15,15 @@ Db = Database(db_name)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Utility to parse Excel')
-    parser.add_argument('-f', '--input', help="File for parsing", dest='file', required=True )
-    parser.add_argument('-o', '--output', help="Total output", dest='output', required=False, default="output.xlsx" )
-    parser.add_argument('-m', '--mode', help="Append or overwrite data", dest='mode', required=False, default="APPEND" )
+    parser.add_argument('-f', '--input', help="File for parsing", dest='file', required=True)
+    parser.add_argument('-o', '--output', help="Total output", dest='output', required=False, default="output.xlsx")
+    parser.add_argument('-m', '--mode', help="Append or overwrite data", dest='mode', required=False, default="APPEND")
+    parser.add_argument('-d', '--delcompany', help="Delete Company by Name", dest='delcomp', required=False, default="")
     arguments = parser.parse_args()
     return arguments
 
 
-def load_data(file : str):
+def load_data(file: str):
     df = pandas.ExcelFile(file)
     sheet1 = df.parse(0)
     type_1_data_start = -1
@@ -36,7 +37,7 @@ def load_data(file : str):
     for el_ in sheet1.values:
         try:
             if int(el_[0]) > 0:
-                day = f"0{str(int(el_[0])%28+1)}"
+                day = f"0{str(int(el_[0]) % 28 + 1)}"
                 dt = f"{year}-{mnth}-{day[-2:]}"
                 comp = el_[1]
                 for j, type_ in enumerate(types):
@@ -48,12 +49,13 @@ def load_data(file : str):
                     else:
                         tp_1 = type_2_data_start
                         tp_2 = len(el_)
-                    for i in range(tp_1,tp_2):
-                        if i in range(tp_1,tp_1+(tp_2 - tp_1)//2):
+                    for i in range(tp_1, tp_2):
+                        if i in range(tp_1, tp_1 + (tp_2 - tp_1) // 2):
+                            # The number of data (data1, data2) for each type can be different.
                             qliq.append(el_[i])
-                        elif i in range(tp_1+(tp_2 - tp_1)//2,tp_2):
+                        elif i in range(tp_1 + (tp_2 - tp_1) // 2, tp_2):
                             qoil.append(el_[i])
-                    if Db.add_data(company=comp,type=type_,qliq=qliq,qoil=qoil,dt=dt) < 0:
+                    if Db.add_data(company=comp, type=type_, qliq=qliq, qoil=qoil, dt=dt) < 0:
                         print("Please check structure of input file")
                         exit(-1)
         except:
@@ -69,6 +71,8 @@ def main():
     print("Started parsing")
     if args.mode.lower() != "append":
         Db.truncate_data()
+    if args.delcomp.lower() != "":
+        Db.delete_company(args.delcomp)
     load_data(args.file)
     try:
         output_data = Db.print_total()
